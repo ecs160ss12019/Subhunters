b8ee4bb6e2dff9afc17b2ad31dbd4481672d995d
rajsbhatia <rsbhatia@ucdavis.edu>
2019-07-07 13:29:44 -0700

    Empty Android Project added to repo

---
+++ app/src/main/java/com/ecs160group/pacman/PacmanActivity.java
@@ -0,0 +1,15 @@
+package com.ecs160group.pacman;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PacmanActivity extends AppCompatActivity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_pacman);
+	}
+}
