abb47929a7493dfd87b06623bf2ada29e584a751
Kevin Nguyen <44216334+kevhn10@users.noreply.github.com>
2019-07-10 23:17:53 -0700

    Added empty Dpad class

---
+++ app/src/main/java/com/ecs160group/pacman/Dpad.java
@@ -0,0 +1,12 @@
+package com.ecs160group.pacman;
+
+/*
+    User's controller for Pacman game
+    Allows user to move left, right, up, and down.
+    Will be located on left side of
+ */
+
+public class Dpad {
+
+
+}
