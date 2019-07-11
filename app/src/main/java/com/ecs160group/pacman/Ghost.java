18701f442eee73674b1af5612a862a04bcf49c29
rajsbhatia <rsbhatia@ucdavis.edu>
2019-07-10 22:14:54 -0700

    Ghost parent class added chase and scatter function stubs

--- app/src/main/java/com/ecs160group/pacman/Ghost.java
+++ app/src/main/java/com/ecs160group/pacman/Ghost.java
@@ -2,4 +2,23 @@ package com.ecs160group.pacman;
 
 public class Ghost
 {
+	/**
+	 * When Pacman isn't super, ghosts chase Pacman
+	 * Overwritten by child classes based on their
+	 * specific behavior
+	 */
+	void chase()
+	{
+
+	}
+
+	/**
+	 * When Pacman is super, ghosts scatter to their
+	 * appropriate corner/coordinate
+	 */
+	void scatter()
+	{
+
+	}
+
 }
