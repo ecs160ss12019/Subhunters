6a2145b0e6dec9996726a655ab5c54e99b62c8d2
nugalm <46472883+nugalm@users.noreply.github.com>
2019-07-10 21:24:54 -0700

    Added empty class Pacman

---
+++ app/src/main/java/com/ecs160group/pacman/Pacman.java
@@ -0,0 +1,13 @@
+package com.ecs160group.pacman;
+
+/*
+    protagonist of Pacman game
+    can eat pellets
+    can eat fruits
+    can eat ghosts when power pellets are eaten
+    must eat all pellets to complete level
+ */
+
+public class Pacman {
+
+}
