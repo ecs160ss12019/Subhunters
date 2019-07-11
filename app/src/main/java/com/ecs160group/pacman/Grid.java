7dbd673d68b944bf80494f35e94c6f49050e1086
rajsbhatia <rsbhatia@ucdavis.edu>
2019-07-10 22:21:25 -0700

    Grid renamed to Maze and no longer extends blocks

--- app/src/main/java/com/ecs160group/pacman/Grid.java
+++
@@ -1,9 +0,0 @@
-package com.ecs160group.pacman;
-
-/*
-  builds the maze for the game/activity 
-  to create paths for movement
- */
-public class Grid extends Blocks
-{
-}
---
+++ app/src/main/java/com/ecs160group/pacman/Maze.java
@@ -0,0 +1,10 @@
+package com.ecs160group.pacman;
+
+/**
+ *   builds the maze for the game/activity
+ *   to create paths for movement, contains a
+ *   bunch of blocks to create the maze
+ */
+public class Maze
+{
+}
