be39c7e26f077211219c83ec71b5d1282d5677fc
Leolithic1st <47678933+Leolithic1st@users.noreply.github.com>
2019-07-13 15:37:12 -0700

    Update Maze.java

--- app/src/main/java/com/ecs160group/pacman/Maze.java
+++ app/src/main/java/com/ecs160group/pacman/Maze.java
@@ -5,6 +5,20 @@ package com.ecs160group.pacman;
  *   to create paths for movement, contains a
  *   bunch of blocks to create the maze
  */
-public class Maze
+public class Maze 
 {
+  //maze set up
+  
+  //print the whole graph
+  
+  //print the wall
+  
+  //print the pacman's position after the last order
+  
+  //using get() function in location and add the fixed value of order direction to the coordinate
+  
+  //print the ghosts
+  
+  //print items (fruits and more)
+  
 }
