481f31fe9d4860b98edb88d03e16558e0146a30f
Leolithic1st <47678933+Leolithic1st@users.noreply.github.com>
2019-07-14 20:04:39 -0700

    blank maze currently

--- app/src/main/java/com/ecs160group/pacman/Maze.java
+++ app/src/main/java/com/ecs160group/pacman/Maze.java
@@ -1,24 +1,34 @@
-package com.ecs160group.pacman;
+package com.example.pacman;
 
-/**
- *   builds the maze for the game/activity
- *   to create paths for movement, contains a
- *   bunch of blocks to create the maze
- */
-public class Maze 
-{
-  //maze set up
-  
-  //print the whole graph
-  
-  //print the wall
-  
-  //print the pacman's position after the last order
-  
-  //using get() function in location and add the fixed value of order direction to the coordinate
-  
-  //print the ghosts
-  
-  //print items (fruits and more)
-  
+import android.graphics.Color;
+import android.util.Log;
+
+class Maze {
+
+
+    private PacmanActivity pacmanActivity;
+
+    public Maze(PacmanActivity pacmanActivity) {
+        this.pacmanActivity = pacmanActivity;
+    }
+
+    //maze set up
+    void draw() {
+        pacmanActivity.gameView.setImageBitmap(pacmanActivity.blankBitmap);
+        //print the whole graph
+        pacmanActivity.canvas.drawColor(Color.argb(255, 255, 255, 255));
+
+        //print the wall
+
+        //print the Pacman's position after the last order
+
+        //using get() function in location and add the fixed value of order direction to the coordinate
+
+        //print the ghosts
+
+        //print items (fruits and more)
+
+        //debugging
+        Log.d("Debugging", "In draw");
+    }
 }
