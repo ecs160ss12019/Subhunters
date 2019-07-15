a0aa7a7ada5f3e2197f0d8d7337c9afe9e17f80d
rajsbhatia <rsbhatia@ucdavis.edu>
2019-07-15 14:50:18 -0700

    stashing levelcreator current work before pull

--- app/src/main/java/com/ecs160group/pacman/LevelCreator.java
+++ app/src/main/java/com/ecs160group/pacman/LevelCreator.java
@@ -1,5 +1,24 @@
 package com.ecs160group.pacman;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+
+/**
+ * Used to read in the text file and create a level from
+ * the file
+ */
 public class LevelCreator
 {
+	// File needed to read
+	final File file = new File("\"../res/levels/lvl1.txt\"")
+	// maze object instance needed to create the maze
+	Maze maze;
+	FileInputStream is;
+	BufferedReader br;
+
+
+
+	void ReadFile() {
+
+	}
 }
