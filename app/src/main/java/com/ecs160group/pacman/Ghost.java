c3df05ee1fbdc914c19603969e48324491897048
nugalm <46472883+nugalm@users.noreply.github.com>
2019-07-14 18:26:44 -0700

    Added variables to ghost/pacman
    checkbounds, draw functions, (need maze done)

--- app/src/main/java/com/ecs160group/pacman/Ghost.java
+++ app/src/main/java/com/ecs160group/pacman/Ghost.java
@@ -2,6 +2,31 @@ package com.ecs160group.pacman;
 
 public class Ghost
 {
+	//used to interact to kill pacman or be eaten
+	private Pacman pacman;
+
+
+	//pixel(maze) coords
+	private int x_maze;
+	private int y_maze;
+
+	//ghost coords//directions
+	int x;
+	int y;
+	private int direction;
+	private int next_direction;
+
+
+	//function to check if ghost is within the bounds of the maze
+	void checkBounds(){
+
+	}
+
+	//function to draw the ghosts
+	//can draw all four at once here or can implement functions for each of the four
+	public void draw(){
+
+	}
 	/**
 	 * When Pacman isn't super, ghosts chase Pacman
 	 * Overwritten by child classes based on their
--- app/src/main/java/com/ecs160group/pacman/Pacman.java
+++ app/src/main/java/com/ecs160group/pacman/Pacman.java
@@ -4,10 +4,38 @@ package com.ecs160group.pacman;
     protagonist of Pacman game
     can eat pellets
     can eat fruits
-    can eat ghosts when power pellets are eaten
+    can eat ghosts for a period of time after power pellets are eaten
     must eat all pellets to complete level
  */
 
 public class Pacman {
+    //pixel(maze) coords
+    private int x_maze;
+    private int y_maze;
+
+    //pacman coords//directions
+    int x;
+    int y;
+    private int direction;
+    private int next_direction;
+
+    //function to check if pacman is within bounds (same function as in ghost class)
+    void checkBounds() {
+
+    }
+
+    //draws pacman on screen
+    //will use variable direction later on for image purposes
+    //circle for now
+    public void draw(){
+
+    }
+
+    //will read user input from our dPad(left, right, up, or down) to update pacmans direction
+    //will take in value from dpad.java
+    void checkDirection(){
+
+
+    }
 
 }
