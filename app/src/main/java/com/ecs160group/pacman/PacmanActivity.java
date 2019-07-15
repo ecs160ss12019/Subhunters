36e14a9d55f38d63a8ebbcd5024e2b0b7f4f3167
Leolithic1st <47678933+Leolithic1st@users.noreply.github.com>
2019-07-14 20:05:21 -0700

    Main activity with the maze working

--- app/src/main/java/com/ecs160group/pacman/PacmanActivity.java
+++ app/src/main/java/com/ecs160group/pacman/PacmanActivity.java
@@ -1,15 +1,52 @@
-package com.ecs160group.pacman;
+package com.example.pacman;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
+//import android.app.Activity;
 import android.os.Bundle;
+//import android.view.MotionEvent;
+import android.util.Log;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+//import android.graphics.Paint;
+import android.graphics.Point;
+import android.view.Display;
+import android.widget.ImageView;
+//import java.util.Random;
 
-public class PacmanActivity extends AppCompatActivity
-{
+public class PacmanActivity extends AppCompatActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState)
-	{
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_pacman);
-	}
+    ImageView gameView;
+    Bitmap blankBitmap;
+    Canvas canvas;
+    //Paint paint;
+
+    //variables to initialize
+    int numHorizonPix;
+    int numVerticPix;
+
+    Maze maze = new Maze(this);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        numHorizonPix = size.x;
+        numVerticPix = size.y;
+
+        blankBitmap = Bitmap.createBitmap(numHorizonPix,
+                numVerticPix,
+                Bitmap.Config.ARGB_8888);
+        canvas = new Canvas(blankBitmap);
+        gameView = new ImageView(this);
+        //paint = new Paint();
+
+        setContentView(gameView);
+        Log.d("Debugging", "In onCreate");
+        maze.draw();
+    }
+
+
 }
