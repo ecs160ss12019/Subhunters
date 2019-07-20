95b03e761d2cd08de05bc298f2837c07eec35e51
Yuguang Liang <yugliang@ucdavis.edu>
2019-07-20 00:34:18 -0700

    Add more details and revise typos
    - change the typo in function comments

--- app/src/main/java/com/ecs160group/pacman/Move.java
+++ app/src/main/java/com/ecs160group/pacman/Move.java
@@ -12,11 +12,14 @@ public class Move{
     // function: Pacman_update
     // change the location of Pacman regarding to its current direction
     // and its velocity
-    void Pacman_update(Location l)
+    void Pacman_update(Pacman pac)
     {
         // read the location to a temp location
-
+        Location loc;
+        loc = new Location(pac.loc.getX(), pac.loc.getY(), pac.loc.block);
         // use the velocity to update x and y coordinates
+        // read the velocity floats of x and y
+        // add value to each for next step in location
 
         // return the changed location to Pacman
     }
@@ -24,7 +27,7 @@ public class Move{
     // function: Ghost_update
     // basically the same thing as the Pacman one but need to
     // check which ghost type it is and execute the different update methods
-    void Ghost_update(Location l)
+    void Ghost_update(Ghost ghost)
     {
     }
 
