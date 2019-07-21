8284f0a6791f4eeb87c61a26e9decaad75e264bd
rajsbhatia <rsbhatia@ucdavis.edu>
2019-07-21 15:15:55 -0700

    Ghost class updated to include isInGrid, scatter, chase, and beginning of moveTowardsTarget functions

--- app/src/main/java/com/ecs160group/pacman/Ghost.java
+++ app/src/main/java/com/ecs160group/pacman/Ghost.java
@@ -6,18 +6,26 @@ import android.graphics.RectF;
 
 import java.util.Random;
 
+/**
+ * Ghost class for movement and interaction with Pacman and the maze
+ * Ghost's movement will check for closest space to target but will not choose
+ * behind the ghost, since a ghost cannot make 180 degree turns and will insteda
+ * turn left or right since one will be closer to target if forward is not possible
+ */
 public class Ghost
 {
-	//used to interact to kill pacman or be eaten
-	private Pacman pacman;
-
 
 	//pixel(maze) coords
+	protected Pacman pacman;
 	protected Location loc;
 
 	//ghost coords//directions
 	private int direction;
 	private int next_direction;
+
+	// tells if ghost has been initialized into the grid and is not in waiting room
+	private boolean started;
+
 
 	//the draw image of ghost
 	int ghostImage = 1;
@@ -31,38 +39,47 @@ public class Ghost
 	Paint paint = new Paint();
 
 
-	public Ghost() {
+	public Ghost()
+	{
 
 	}
 
-	public Ghost (int screenX, int locX, int locY) {
+	public Ghost(int screenX, int locX, int locY)
+	{
 
-		paint.setColor(Color.argb(255, 0,0,255));
+		paint.setColor(Color.argb(255, 0, 0, 255));
 		//pacman width/height 1% of screen (change later if needed)
-		mGhostWidth = (float)screenX/100;
-		mGhostHeight = (float)screenX/100;
-
+		mGhostWidth = (float) screenX / 100;
+		mGhostHeight = (float) screenX / 100;
 
 
 		loc = new Location(locX, locY, Block.GHOST);
 
 	}
 
+	private boolean isInGrid()
+	{
+		return started;
+	}
+
 
 	/**
 	 * updates the ball position
 	 * called each frame/loop from PacmanGame update() method
-	 * moves ball based on x/y velocities and fps
+	 * moves ghost based on x/y velocities and fps
 	 */
-	void update(long fps) {
+	void update(long fps)
+	{
 
 	}
 
-	void reverseXVel(){
+	void reverseXVel()
+	{
 		mXVelocity = -mXVelocity;
 	}
 
-	void reverseYVel(){
+	void reverseYVel()
+	{
 		mYVelocity = -mYVelocity;
 	}
 
@@ -70,43 +87,64 @@ public class Ghost
 	 * Initializes four points of mRect(defines pacman)
 	 * Initializes x and y velocities (can change later)
 	 */
-	void reset(int x, int y) {
+	// TODO: not sure if we needs this
+	void reset(int x, int y)
+	{
 
 
-		mXVelocity = (float)(y / 3);
-		mYVelocity = (float)-(y / 3);
+		mXVelocity = (float) (y / 3);
+		mYVelocity = (float) -(y / 3);
 
 
 	}
 
 
 	//function to check if ghost is within the bounds of the maze
-	void checkBounds(){
+	void checkBounds()
+	{
 
 	}
 
 	//function to draw the ghosts
 	//can draw all four at once here or can implement functions for each of the four
-	public void draw(){
+	public void draw()
+	{
 
 	}
+
 	/**
 	 * When Pacman isn't super, ghosts chase Pacman
 	 * Overwritten by child classes based on their
 	 * specific behavior
 	 */
-	void chase()
+	void chase(Location chaseLocation)
 	{
-
+		moveTowardsTarget(chaseLocation);
 	}
 
 	/**
 	 * When Pacman is super, ghosts scatter to their
 	 * appropriate corner/coordinate
 	 */
-	void scatter()
+	void scatter(Location scatterLocation)
 	{
-
+		moveTowardsTarget(scatterLocation);
 	}
 
+	/**
+	 * Private helper used by chase and scatter that moves towards target
+	 *
+	 * @param target target location to head towards
+	 */
+	private void moveTowardsTarget(Location target)
+	{
+		// keep moving towards target if pacman hasn't won and isn't dead
+		if (pacman != null && !pacman.hasWon()) {
+			if (target != null && isInGrid()) { // target exists within grid
+				// TODO: get next location to move to in grid
+				// TODO: if can move/next location exists: move, else: turn
+
+			}
+		}
+	}
 }
