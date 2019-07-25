b58347af8c42279fca40b283e63167ecf6815151
rajsbhatia <rsbhatia@ucdavis.edu>
2019-07-25 13:53:12 -0700

    Blinky class updated with scatter location as well as move, getscatter and getchase location functions

--- app/src/main/java/com/ecs160group/pacman/Blinky.java
+++ app/src/main/java/com/ecs160group/pacman/Blinky.java
@@ -6,4 +6,38 @@ package com.ecs160group.pacman;
  */
 public class Blinky extends Ghost
 {
+	private static final Location SCATTER = new Location(0, 27, Block.WALL);
+
+	/**
+	 * Gets Blinky's scatter Location
+	 * @return the scatter target
+	 */
+	public Location getScatterLocation()
+	{
+		return SCATTER;
+	}
+
+	/**
+	 * Gets the chase location for Blinky to chase to
+	 * Blinky's chase location is pacman's current location
+	 *
+	 * @return pacman's location, Blinky's location to move
+	 * towards when Pacman isn't super
+	 */
+	public Location getChaseLocation()
+	{
+		return pacman.getLoc();
+	}
+
+	/**
+	 * Controls Blinky's movement around the board
+	 */
+	public void move()
+	{
+		if (pacman.isSuper()) {
+			scatter(SCATTER);
+		} else {
+			chase(getChaseLocation());
+		}
+	}
 }
