bfa59ca7d7517adf2bfb7351800eab2f8bda15ca
rajsbhatia <rsbhatia@ucdavis.edu>
2019-07-28 14:07:58 -0700

    Score class updated with functions to update score for eating various things

--- app/src/main/java/com/ecs160group/pacman/Score.java
+++ app/src/main/java/com/ecs160group/pacman/Score.java
@@ -2,4 +2,55 @@ package com.ecs160group.pacman;
 
 public class Score
 {
+	int score;
+
+	/**
+	 * Default ctor for the score class
+	 */
+	Score()
+	{
+		score = 0;
+	}
+
+	/**
+	 *
+	 * @return
+	 */
+	int getScore()
+	{
+		return score;
+	}
+
+	/**
+	 * Called when Pacman eats a pellet
+	 */
+	void atePellet()
+	{
+		score += 10;
+	}
+
+	/**
+	 *  Called when Pacman eats a power pellet
+	 */
+	void atePowerPellet()
+	{
+		score += 100;
+	}
+
+	/**
+	 * Called when Pacman eats a ghost
+	 */
+	void ateGhost()
+	{
+		score += 500;
+	}
+
+	/**
+	 * Called when Pacman eats a fruit
+	 */
+	// TODO: extend this for multiple fruit types
+	void ateFruit()
+	{
+		score += 1000;
+	}
 }
