cce83fb8844e1d825f02fc24a2557e215e9a1630
rajsbhatia <rsbhatia@ucdavis.edu>
2019-07-28 21:46:22 -0700

    Collision interface updated function stubs

--- app/src/main/java/com/ecs160group/pacman/Collision.java
+++ app/src/main/java/com/ecs160group/pacman/Collision.java
@@ -3,10 +3,6 @@ package com.ecs160group.pacman;
 // used to detect all collisions between Pacman and
 // ghosts, fruit, pullets, and wall
 public interface Collision {
-    public void isInBounds(int mScreenX, int mScreenY);
-    public boolean detectCollision(Location loc, int mScreenX, int mScreenY);
+    public boolean hasCollision(Location l);
     public boolean wallDetection(Maze maze);
-
-
-
 }
